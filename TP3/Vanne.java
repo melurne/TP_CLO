d2e406682997ffc046af43547efb47b1768cd2a1
melurne <maxence1.neus@gmail.com>
2022-02-08 16:54:12 +0100

    Correction

--- TP3/Vanne.java
+++ TP3/Vanne.java
@@ -11,6 +11,12 @@ public class Vanne extends Composant {
 		in = comp;
 	}
 
+	public String description() {
+		
+		return super.description() + " in1: " + (in == null ? "not connected" : in.getId());
+
+	}
+
 	public boolean getEtat() throws NonConnectedException {
 		if (in == null) {
 			throw new NonConnectedException();
